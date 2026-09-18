ab0213f7655902f026d482c4b27b81cc12d267fa
Turki alyahyai <turki.k69@gmail.com>
2026-09-18 17:07:06 +0400

    Deploy menu and username login fix: service-worker.js

--- warraq/service-worker.js
+++ warraq/service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "warraq-shell-v12";
+const CACHE_NAME = "warraq-shell-v13";
 const RUNTIME_CACHE = "warraq-runtime-v1";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app.js?v=20260918-7",
-  "./styles.css?v=20260918-9",
+  "./app.js?v=20260918-10",
+  "./styles.css?v=20260918-10",
   "./privacy.html",
   "./terms.html"
 ];
