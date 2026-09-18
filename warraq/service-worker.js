ac88d195dc53f31c6d36749bd851da5267d07c95
Turki alyahyai <turki.k69@gmail.com>
2026-09-18 15:39:29 +0400

    Deploy Warraq home v3: service-worker.js

--- warraq/service-worker.js
+++ warraq/service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "warraq-shell-v8";
+const CACHE_NAME = "warraq-shell-v9";
 const RUNTIME_CACHE = "warraq-runtime-v1";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app.js?v=20260918-5",
-  "./styles.css?v=20260918-5",
+  "./app.js?v=20260918-6",
+  "./styles.css?v=20260918-6",
   "./privacy.html",
   "./terms.html"
 ];
