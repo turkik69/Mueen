cfee45cd195d8a3803779265a20114b7da71f75b
Turki alyahyai <turki.k69@gmail.com>
2026-09-18 15:08:34 +0400

    Deploy legacy account fix: service-worker.js

--- warraq/service-worker.js
+++ warraq/service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "warraq-shell-v6";
+const CACHE_NAME = "warraq-shell-v7";
 const RUNTIME_CACHE = "warraq-runtime-v1";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app.js?v=20260918-3",
-  "./styles.css?v=20260918-3",
+  "./app.js?v=20260918-4",
+  "./styles.css?v=20260918-4",
   "./privacy.html",
   "./terms.html"
 ];
