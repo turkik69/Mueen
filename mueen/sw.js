eef4a1c105ac0c46d99d4390071ae20982e48e25
Turki alyahyai <turki.k69@gmail.com>
2026-09-19 20:31:42 +0400

    Update Mueen service worker Firebase config

--- mueen/sw.js
+++ mueen/sw.js
@@ -1,16 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
 firebase.initializeApp({
-  apiKey:"AIzaSyBsnryD1ZtvjzumatCCVN-QpRAMR4_IG7M",
-  authDomain:"world-cup-2026-d3091.firebaseapp.com",
-  databaseURL:"https://world-cup-2026-d3091-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId:"world-cup-2026-d3091",
-  storageBucket:"world-cup-2026-d3091.firebasestorage.app",
-  messagingSenderId:"830204361101",
-  appId:"1:830204361101:web:f3a23c0fa41bb809d365c4"
+  apiKey:"AIzaSyDZ5MYN5z1FiSVe9LoYXmER4NUUkW6C0us",
+  authDomain:"fitness-coach-21b40.firebaseapp.com",
+  databaseURL:"https://fitness-coach-21b40-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId:"fitness-coach-21b40",
+  storageBucket:"fitness-coach-21b40.firebasestorage.app",
+  messagingSenderId:"162393884721",
+  appId:"1:162393884721:web:d0d5768c827b997c322d10"
 });
 const messaging=firebase.messaging();
-const CACHE='mueen-v2';
+const CACHE='mueen-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./firebase-adapter.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
